7ddd7ede1192be6fa5c591d8d942869b5429e80d
AndyEngland521 <andrew.england@sparkfun.com>
2018-11-19 10:14:54 -0700

    updating examples

--- examples/Example1_BasicReadings/Example1_BasicReadings.ino
+++ examples/Example1_BasicReadings/Example1_BasicReadings.ino
@@ -26,7 +26,7 @@ void setup() {
   Wire.begin();
   Serial.begin(115200);
   
-  if (fingerSensor.begin(Wire, 100000, ADS1015_ADDRESS_VDD, 9600) == false) {
+  if (fingerSensor.begin(Wire, 100000, ADS1015_ADDRESS_GND) == false) {
      Serial.println("Device not found. Check wiring.");
      while (1);
   } 
--- examples/Example3_Calibration/Example3_Calibration.ino
+++ examples/Example3_Calibration/Example3_Calibration.ino
@@ -26,7 +26,7 @@ void setup() {
   Wire.begin();
   Serial.begin(115200);
   
-  if (fingerSensor.begin(Wire, 100000, ADS1015_ADDRESS_SDA) == false) {
+  if (fingerSensor.begin(Wire, 100000, ADS1015_ADDRESS_GND) == false) {
      Serial.println("Device not found. Check wiring.");
      while (1);
   }
